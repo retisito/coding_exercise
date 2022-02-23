3469b62558fdce39ee505507e10f1dff78ce3b12
Adrian Chacon <retisito@gmail.com>
2022-02-23 09:44:50 -0400

    Fix some misstypes

--- word_counter_test.rb
+++ word_counter_test.rb
@@ -16,14 +16,14 @@ class WordCounterTest < Minitest::Test
     assert_equal output, WordCounter.counter(input)
   end
 
-  def test_when_the_input_has_an_number
+  def test_when_the_input_has_a_number
     #skip
     input = "I will count until ten 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10"
     output = { i:1, will:1, count:1, until:1, ten:1 }
     assert_equal output, WordCounter.counter(input)
   end
 
-  def test_when_the_input_has_word_with_an_number
+  def test_when_the_input_has_word_with_a_number
     #skip
     input = "This word is no valid word beacuse it has a number: S0top"
     output = { this:1, word:2, is:1, no:1, valid:1, beacuse:1, it:1, has:1, a:1, number:1, s:1, top:1}
